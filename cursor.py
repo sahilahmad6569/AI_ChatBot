d63999a60d6a4335c2ee83c0beb18e3a54193e01
Sahil Ahmad <sahilahmad6569@gmail.com>
2024-07-05 15:16:54 +0530

    Add cursor.py

---
+++ cursor.py
@@ -0,0 +1,5 @@
+# This script is only meant to find the accurate coordinates of the screen in order to make the ChatBot work properly
+import pyautogui
+
+while True:
+    print(pyautogui.position())
